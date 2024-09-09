f8c5891230d093955feaf3940f2de5cc2b29eb7d
gbemzprogram1 <135862484+gbemzprogram1@users.noreply.github.com>
2024-09-09 00:18:20 -0400

    Made a mistake used the amount the car can go per liter to calculate the total cost instead of getting input on the distance to work

--- costbenefit.py
+++ costbenefit.py
@@ -11,8 +11,11 @@ resale= costofcar-sellcar
 #knowing how much distance the car can go with a single liter
 kilometers=float(input("How many kilometers do you get per liter? "))
 
+#distance to work
+distance= float(input("How many kilometers is it for you to get to work? "))
+
 #total kilometers = number of kilometers * 2 trips per day * 5 days a week * 47 weeks
-totalkilo= kilometers * 2 * 5 * 47
+totalkilo= distance * 2 * 5 * 47
 
 #the base price of gas
 basecost=float(input("How much does gas cost per litre? $"))
