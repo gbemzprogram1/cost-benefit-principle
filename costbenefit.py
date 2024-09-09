bf7f6ee1691f352732efcb9db612c770692183c8
gbemzprogram1 <135862484+gbemzprogram1@users.noreply.github.com>
2024-09-08 23:48:14 -0400

    got the total amount that would be spent over the course of the year on the car

--- costbenefit.py
+++ costbenefit.py
@@ -6,6 +6,7 @@ costofcar=float(input("How much is the car you want to buy? $"))
 
 #selling price of the car after a year of use
 sellcar= float(input("How much can the car be sold for after a year of use? $"))
+resale= costofcar-sellcar
 
 #knowing how much distance the car can go with a single liter
 kilometers=float(input("How many kilometers do you get per liter? "))
@@ -16,7 +17,24 @@ totalkilo= kilometers * 2 * 5 * 47
 #the base price of gas
 basecost=float(input("How much does gas cost per litre? "))
 
+#total cost of gas for a year
 gascost= totalkilo/kilometers *basecost
+
+#cost for parking in a year
+parkcost= float(input("How much does parking cost for a day? $"))
+
+parkcost=parkcost * 5 * 47
+
+#insurance and registration
+insurance= float(input("What would be the cost of your parking and registration? $"))
+
+#repairs for a year
+repairs= float(input("How much do you think repairs of the car would cost for the year? $"))
+
+#total amount that would be spent on the car over the course of a year
+totalcar= repairs + insurance + parkcost + gascost + resale
+
+
 
 
 
