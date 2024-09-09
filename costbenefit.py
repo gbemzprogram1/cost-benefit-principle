8fb60845dc53b3185d01696165437e38e91aae0e
gbemzprogram1 <135862484+gbemzprogram1@users.noreply.github.com>
2024-09-08 23:37:13 -0400

    added some input functions to get information from the user, calculated the totalnumber of kilometers a year the car would go

--- costbenefit.py
+++ costbenefit.py
@@ -2,6 +2,21 @@
 
 
 #cost of the car
-costofcar=input("How much is the car you want to buy? ")
+costofcar=float(input("How much is the car you want to buy? $"))
 
-sellcar= input("How much can the car be sold for after a year of use? ")
+#selling price of the car after a year of use
+sellcar= float(input("How much can the car be sold for after a year of use? $"))
+
+#knowing how much distance the car can go with a single liter
+kilometers=float(input("How many kilometers do you get per liter? "))
+
+#total kilometers = number of kilometers * 2 trips per day * 5 days a week * 47 weeks
+totalkilo= kilometers * 2 * 5 * 47
+
+#the base price of gas
+gascost=float(input("How much does gas cost per litre? "))
+
+
+
+
+
