d6bdad7e5617518792cdda67dfc553cb7f9ace2a
gbemzprogram1 <135862484+gbemzprogram1@users.noreply.github.com>
2024-09-08 23:23:38 -0400

    starting input to get the cost of the car

--- costbenefit.py
+++ costbenefit.py
@@ -1,2 +1,7 @@
 #learning economics and just using this for some documentation
 
+
+#cost of the car
+costofcar=input("How much is the car you want to buy? ")
+
+sellcar= input("How much can the car be sold for after a year of use? ")
