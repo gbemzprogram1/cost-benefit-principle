c25d1b19af185ac38c674e17da842769f698ac75
gbemzprogram1 <135862484+gbemzprogram1@users.noreply.github.com>
2024-09-08 23:15:26 -0400

    rewriting the code to match an example that shows the cost of using an uber vs buying a car

--- costbenefit.py
+++ costbenefit.py
@@ -1,10 +1,2 @@
-#cost benefit checker for if the value is worth it to you or not
+#learning economics and just using this for some documentation
 
-cost=int(input("What is the price of what you want to buy?  "))
-benefit=int(input("What is the benefit of this to you?, what is the maximum you would be willing to pay to fulfil the desire "))
-
-if cost > benefit:
-    print("think again before buying this")
-    
-else:
-    print('You can go ahead with your purchase since they are of equal value to you or the benefiit seems greater to you')    
